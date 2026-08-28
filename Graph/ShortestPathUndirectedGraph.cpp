156eac19fd07319fa9ec12ef48e6c229af6b4e2b
sahil <b200091@nitsikkim.ac.in>
2026-08-28 09:31:15 +0530

    Updated shorted part in undirected graph

--- Graph/ShortestPathUndirectedGraph.cpp
+++ Graph/ShortestPathUndirectedGraph.cpp
@@ -1,178 +1,90 @@
-// #include<vector>
-// #include<unordered_map>
-// #include<list>
-// #include<iostream>
-// #include<queue>
-// using namespace std;
-
-// vector<int> ShortestPath(vector<vector<int>>&edges, int m, int n, int FirstNd, int SecNd){
-// //Creating Adjacency List
-
-// unordered_map<int, list<int>> Adj;
-// for(int i = 0; i < m; i++){
-//     int u = edges[i][0];
-//     int v = edges[i][1];
-
-//     Adj[u].push_back(v);
-//     Adj[v].push_back(u);
-// }
-// //Visited 
-// unordered_map<int, bool>Visited;
-// //Parents Tracking
-// unordered_map<int , int> Parents;
-
-// //BFS Queues
-// queue<int>q;
-// //for disconnect graph
-
-// q.push(FirstNd);
-// Visited[FirstNd] = true;
-
-// Parents[FirstNd] = -1;
-
-// while(!q.empty()){
-//     int front = q.front();
-//     q.pop();
-//     for(auto neighbour : Adj[front]){
-
-//         if(!Visited[neighbour]){
-//             Visited[neighbour] = true;
-//             Parents[neighbour] = front;
-//             q.push(neighbour);
-//         }
-        
-//     }    
-// }
-
-//     vector<int>ans;
-//     for(int i = 0; i< n; i++){
-//         bool AnsPath = false;
-//         if(Parents[i] == FirstNd || AnsPath){
-//             ans.push_back(i);
-//             AnsPath = true;
-//         }
-//         if(Parents[i] = SecNd){
-//             AnsPath = false;
-//         }
-//     }
-//     return ans;
-
-// }
-
-// int main(){
-// vector<vector<int>> edges{
-//     {1,2},
-//     {1,3},
-//     {1,4},
-//     {2,3},
-//     {3,8},
-//     {4,6}, 
-//     {5,8},{6,7}, {7,8}
-// };    vector<int > Path = ShortestPath(edges, 9, 8, 1, 8);
-//     for(int i = 0; i<5; i++){
-//     cout << Path[i] << " ";
-// }
-// }
-
-#include <vector>
-#include <unordered_map>
-#include <list>
-#include <iostream>
-#include <queue>
-#include <algorithm>
-
+#include<vector>
+#include<unordered_map>
+#include<list>
+#include<iostream>
+#include<algorithm>
+#include<queue>
 using namespace std;
 
-vector<int> ShortestPath(vector<vector<int>>& edges, int m, int n,
-                         int FirstNd, int SecNd)
-{
-    // Creating Adjacency List
-    unordered_map<int, list<int>> Adj;
+vector<int> ShortestPath(vector<vector<int>>&edges, int m, int n, int FirstNd, int SecNd){
+//Creating Adjacency List
 
-    for(int i = 0; i < m; i++)
-    {
-        int u = edges[i][0];
-        int v = edges[i][1];
+unordered_map<int, list<int>> Adj;
+for(int i = 0; i < m; i++){
+    int u = edges[i][0];
+    int v = edges[i][1];
 
-        Adj[u].push_back(v);
-        Adj[v].push_back(u);
-    }
+    Adj[u].push_back(v);
+    Adj[v].push_back(u);
+}
+//Visited 
+unordered_map<int, bool>Visited;
+//Parents Tracking
+unordered_map<int , int> Parents;
 
-    // Visited
-    unordered_map<int, bool> Visited;
+//BFS Queues
+queue<int>q;
+//for disconnect graph
 
-    // Parents Tracking
-    unordered_map<int, int> Parents;
+q.push(FirstNd);
+Visited[FirstNd] = true;
 
-    // BFS Queue
-    queue<int> q;
+Parents[FirstNd] = -1;
 
-    q.push(FirstNd);
-    Visited[FirstNd] = true;
-    Parents[FirstNd] = -1;
+while(!q.empty()){
+    int front = q.front();
+    q.pop();
+    if(front == SecNd)break;
+    for(auto neighbour : Adj[front]){
 
-    // BFS
-    while(!q.empty())
-    {
-        int front = q.front();
-        q.pop();
-
-        for(auto neighbour : Adj[front])
-        {
-            if(!Visited[neighbour])
-            {
-                Visited[neighbour] = true;
-                Parents[neighbour] = front;
-
-                q.push(neighbour);
-            }
+        if(!Visited[neighbour]){
+            Visited[neighbour] = true;
+            Parents[neighbour] = front;
+            q.push(neighbour);
         }
-    }
-
-    // If destination was not reached
-    if(!Visited[SecNd])
-    {
-        return {};
-    }
-
-    // Reconstruct path
-    vector<int> ans;
-
-    int current = SecNd;
-
-    while(current != -1)
-    {
-        ans.push_back(current);
-        current = Parents[current];
-    }
-
-    // Reverse path
-    reverse(ans.begin(), ans.end());
-
-    return ans;
+        
+    }    
 }
 
-int main()
-{
-    vector<vector<int>> edges
-    {
-        {1,2},
-        {1,3},
-        {1,4},
-        {2,3},
-        {3,8},
-        {4,6},
-        {5,8},
-        {6,7},
-        {7,8}
-    };
+    vector<int>ans;
+    int current = SecNd;
+    if(!Visited[SecNd]) return ans;
 
-    vector<int> Path = ShortestPath(edges, 9, 8, 1, 8);
+        while (current != -1){
+            ans.push_back(current);
+            current = Parents[current];
+        }
+        {
+            /* code */
+        }
+        
+    // for(int i = 0; i< n; i++){
+    //     bool AnsPath = false;
+    //     if(Parents[i] == FirstNd || AnsPath){
+    //         ans.push_back(i);
+    //         AnsPath = true;
+    //     }
+    //     if(Parents[i] = SecNd){
+    //         AnsPath = false;
+    //     }
+    // }
+       reverse(ans.begin(), ans.end());
+    return ans;
 
-    for(int node : Path)
-    {
-        cout << node << " ";
-    }
+}
 
-    return 0;
+int main(){
+vector<vector<int>> edges{
+    {1,2},
+    {1,3},
+    {1,4},
+    {2,3},
+    {2,5},
+    {3,8},
+    {4,6}, 
+    {5,8},{6,7}, {7,8}
+};    vector<int > Path = ShortestPath(edges, 9, 8, 1, 8);
+    for(int i = 0; i<5; i++){
+    cout << Path[i] << " ";
+}
 }
